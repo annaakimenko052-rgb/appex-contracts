2508ae096f8959a6ba7946e18fc2af5f43493a26
annaakimenko052-rgb <annaakimenko052@gmail.com>
2026-07-10 17:46:11 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['docxtemplater', 'pizzip']
+  }
+}
+
+module.exports = nextConfig
